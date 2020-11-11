60e9e966f168f1b10e80336e916c25002309a034
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2020-11-11 19:24:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -260,9 +260,6 @@ app.post("/submit", function (req, res) {
  
 })
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000.");
-
-
-  
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server started");
 });
